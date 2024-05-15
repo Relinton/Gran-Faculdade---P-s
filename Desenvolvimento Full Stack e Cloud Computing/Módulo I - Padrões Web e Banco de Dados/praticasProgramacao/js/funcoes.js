27ab87acbccc8a41c97c3f9daa37e127f6aa622b
Relinton Pinheiro <relintonproande@gmail.com>
2024-05-14 22:59:51 -0300

    Praticando JS: arrays, objetos, funções declarativas, expressivas, arrows explícitas, implícitas, recursividade, etc...

---
+++ Desenvolvimento Full Stack e Cloud Computing/Módulo I - Padrões Web e Banco de Dados/praticasProgramacao/js/funcoes.js
@@ -0,0 +1,61 @@
+function calcular(operacao) {
+    const n1 = Number(document.getElementById('n1').value)
+    const n2 = Number(document.getElementById('n2').value)
+
+    let resultado;
+    switch(operacao) {
+        case '+': resultado = somar(n1, n2);
+        break;
+        case '-': resultado = subtrair(n1, n2);
+        break;
+        case '*': resultado = multiplicar(n1, n2);
+        break;
+        case '/': resultado = dividir(n1, n2);
+        break;
+    }
+
+    document.getElementById('resultado').innerHTML = 'O resultado é ' + resultado
+}
+
+function somar(n1, n2) {
+    return Number(n1) + Number(n2)
+}
+
+function subtrair(n1, n2) {
+    return Number(n1) - Number(n2)
+}
+
+function multiplicar(n1, n2) {
+    return Number(n1) * Number(n2)
+}
+
+function dividir(n1, n2) {
+    if (n2 == 0) {
+        console.log('Não é possível dividir número por 0')
+        return null
+    } else {
+        return Number(n1) / Number(n2)
+    }
+}
+
+
+const somaExpressao = function(n1, n2) {
+    return n1 + n2
+}
+console.log(somaExpressao(10, 23))
+
+
+
+const somaArrowComUmParametro = n1 => {
+    return n1 + 4
+}
+
+const somaArrowComDoisParametrosComRetornoExplicito = (n1, n2) => {
+    return n1 + n2
+}
+
+const somaArrowComDoisParametrosComRetornoImplicito = (n1, n2) => (
+    n1 + n2
+)
+
+const somaArrowEmUmaLinha = (n1, n2) => n1 + n2
